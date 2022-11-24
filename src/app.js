792387e99c509a97af25074ff1432224065136e0
sophharris1998 <117087805+sophharris1998@users.noreply.github.com>
2022-11-24 22:31:32 +0000

    added changes

--- src/app.js
+++ src/app.js
@@ -1,3 +1,35 @@
 import "./style.scss";
+import confetti from "canvas-confetti";
+import colorthief from "colorthief";
 
+const colorThief = new colorthief();
 console.log("Welcome to NPM!");
+const confettiButton = document.querySelector(".confettiFire");
+const dogImage = document.querySelector("#dog-image");
+const imgAdd = document.querySelector("#imgAdd");
+
+console.log(dogImage);
+
+const confettiFire = () => {
+  confetti();
+};
+
+const onImageLoad = () => {
+  const color = colorThief.getColor(dogImage);
+
+  const body = document.querySelector("body");
+
+  body.style.backgroundColor = `rgb(${color[0]},${color[1]},${color[2]})`;
+};
+
+confettiButton.addEventListener("click", confettiFire);
+if (dogImage.complete) {
+  onImageLoad();
+} else {
+  dogImage.addEventListener("load", onImageLoad);
+}
+
+const newImg = () => {};
+imgAdd.addEventListener("click", newImg);
+
+console.log("hello");
